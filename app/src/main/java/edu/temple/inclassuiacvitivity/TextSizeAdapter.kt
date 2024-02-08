bc7768809c6ab46dde2d5a8835b6c1889b7c59ac
Niaz Baharudeen <tuk05348@temple.edu>
2024-02-08 11:58:49 -0500

    Implemented getDropDownView functionality, has dynamic text size, changed getView functionality has static text size.

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
+import org.w3c.dom.Text
 
 class TextSizeAdapter(_context: Context, _numbers: Array<Int>) : BaseAdapter() {
 
@@ -31,9 +32,22 @@ class TextSizeAdapter(_context: Context, _numbers: Array<Int>) : BaseAdapter() {
             textView = TextView(context)
         }
         textView.text = numbers[position].toString()
-        textView.textSize = numbers[position].toFloat()
+        textView.textSize = 20f
         textView.setPadding(5, 10, 0, 10)
         return textView
     }
 
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val textView : TextView
+        if(convertView != null){
+            textView = convertView as TextView
+        }
+        else{
+            textView = TextView(context)
+        }
+        textView.text = numbers[position].toString()
+        textView.textSize = numbers[position].toFloat()
+        textView.setPadding(5, 10, 0, 10)
+        return textView
+    }
 }
