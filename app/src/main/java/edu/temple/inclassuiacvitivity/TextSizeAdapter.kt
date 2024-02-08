bde4c2c3d27678e36f82a2e94b24e6ee89cb9833
Niaz Baharudeen <tuk05348@temple.edu>
2024-02-08 11:31:59 -0500

    Impletemented getCount, getItem, and getItemID for custom adapter.

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -1,21 +1,24 @@
 package edu.temple.inclassuiacvitivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 
-class TextSizeAdapter : BaseAdapter() {
+class TextSizeAdapter(_context: Context, _numbers: Array<Int>) : BaseAdapter() {
 
+    val context = _context
+    val numbers = _numbers
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return numbers.size
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+        return numbers[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+        return position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
